5ce0fbc9c6135317760ef4a6794eeb660ce48c9d
Max Graw <github@maxgraw.com>
2024-11-02 17:09:24 +0100

    save

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from "astro:content";
+
+import { glob } from "astro/loaders";
+
+const recipes = defineCollection({
+	loader: glob({ pattern: "**/*.json", base: "./src/data/recipes" }),
+	schema: z.object({
+		title: z.string(),
+		ingredients: z.array(
+			z.object({
+				name: z.string(),
+				amount: z.string().optional(),
+			}),
+		),
+		steps: z.array(
+			z.object({
+				content: z.string(),
+			}),
+		),
+	}),
+});
+
+export const collections = { recipes };
