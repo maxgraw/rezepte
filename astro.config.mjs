09da355496f39e631150669a6c4fe80c17a4356d
Max Graw <github@maxgraw.com>
2024-11-16 23:47:14 +0100

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import pagefind from "./src/integration/pagefind";
 import { defineConfig } from "astro/config";
+import path from "node:path";
+
+const __dirname = import.meta.dirname;
 
 // https://astro.build/config
 export default defineConfig({
