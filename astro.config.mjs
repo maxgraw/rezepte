17154aa3ce349e0b550cf60ed9215e13c1de0f9a
Max Graw <github@maxgraw.com>
2024-11-17 16:09:39 +0100

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://rezepte.maxgraw.com',
     build: {
         format: "file",
     },
-    integrations: [pagefind(), sitemap()],
+    integrations: [pagefind(), sitemap(), purgecss()],
 });
