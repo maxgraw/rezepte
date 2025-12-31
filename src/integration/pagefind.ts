5b4514810ace9eebc12349cdaee14c9fda1b66a5
Max Graw <github@maxgraw.com>
2025-12-31 11:59:47 +0100

    fixes

--- src/integration/pagefind.ts
+++ src/integration/pagefind.ts
@@ -5,73 +5,73 @@ import { createIndex } from "pagefind";
 import sirv from "sirv";
 
 export default function pagefind(): AstroIntegration {
-	let outDir: string;
-	return {
-		name: "pagefind",
-		hooks: {
-			"astro:config:setup": ({ config }) => {
-				outDir = fileURLToPath(config.outDir);
-			},
-			"astro:server:setup": ({ server, logger }) => {
-				if (!outDir) {
-					logger.warn(
-						"astro-pagefind couldn't reliably determine the output directory. Search assets will not be served.",
-					);
-					return;
-				}
+  let outDir: string;
+  return {
+    name: "pagefind",
+    hooks: {
+      "astro:config:setup": ({ config }) => {
+        outDir = fileURLToPath(config.outDir);
+      },
+      "astro:server:setup": ({ server, logger }) => {
+        if (!outDir) {
+          logger.warn(
+            "astro-pagefind couldn't reliably determine the output directory. Search assets will not be served.",
+          );
+          return;
+        }
 
-				const serve = sirv(outDir, {
-					dev: true,
-					etag: true,
-				});
-				server.middlewares.use((req, res, next) => {
-					if (req.url?.startsWith("/pagefind/")) {
-						serve(req, res, next);
-					} else {
-						next();
-					}
-				});
-			},
-			"astro:build:done": async ({ logger }) => {
-				if (!outDir) {
-					logger.warn(
-						"astro-pagefind couldn't reliably determine the output directory. Search index will not be built.",
-					);
-					return;
-				}
+        const serve = sirv(outDir, {
+          dev: true,
+          etag: true,
+        });
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.startsWith("/pagefind/")) {
+            serve(req, res, next);
+          } else {
+            next();
+          }
+        });
+      },
+      "astro:build:done": async ({ logger }) => {
+        if (!outDir) {
+          logger.warn(
+            "astro-pagefind couldn't reliably determine the output directory. Search index will not be built.",
+          );
+          return;
+        }
 
-				const { index, errors: createErrors } = await createIndex({});
-				if (!index) {
-					logger.error("Pagefind failed to create index");
-					for (const error of createErrors) {
-						logger.error(error);
-					}
-					return;
-				}
-				const { page_count, errors: addErrors } = await index.addDirectory({
-					path: outDir,
-				});
-				if (addErrors.length) {
-					logger.error("Pagefind failed to index files");
-					for (const error of addErrors) {
-						logger.error(error);
-					}
-					return;
-				}
-				logger.info(`Pagefind indexed ${page_count} pages`);
+        const { index, errors: createErrors } = await createIndex({});
+        if (!index) {
+          logger.error("Pagefind failed to create index");
+          for (const error of createErrors) {
+            logger.error(error);
+          }
+          return;
+        }
+        const { page_count, errors: addErrors } = await index.addDirectory({
+          path: outDir,
+        });
+        if (addErrors.length) {
+          logger.error("Pagefind failed to index files");
+          for (const error of addErrors) {
+            logger.error(error);
+          }
+          return;
+        }
+        logger.info(`Pagefind indexed ${page_count} pages`);
 
-				const { outputPath, errors: writeErrors } = await index.writeFiles({
-					outputPath: path.join(outDir, "pagefind"),
-				});
-				if (writeErrors.length) {
-					logger.error("Pagefind failed to write index");
-					for (const error of writeErrors) {
-						logger.error(error);
-					}
-					return;
-				}
-				logger.info(`Pagefind wrote index to ${outputPath}`);
-			},
-		},
-	};
+        const { outputPath, errors: writeErrors } = await index.writeFiles({
+          outputPath: path.join(outDir, "pagefind"),
+        });
+        if (writeErrors.length) {
+          logger.error("Pagefind failed to write index");
+          for (const error of writeErrors) {
+            logger.error(error);
+          }
+          return;
+        }
+        logger.info(`Pagefind wrote index to ${outputPath}`);
+      },
+    },
+  };
 }
--- src/utils/tags.ts
+++ src/utils/tags.ts
@@ -1,31 +1,31 @@
 type TagData = {
-	original: string;
-	lowercase: string;
-	count: number;
+  original: string;
+  lowercase: string;
+  count: number;
 };
 
 type Recipe = {
-	data: {
-		tags?: string[];
-	};
+  data: {
+    tags?: string[];
+  };
 };
 
 export const get_unique_tags = (recipes: Recipe[]): TagData[] => {
-	return recipes
-		.flatMap((recipe) => recipe.data.tags || [])
-		.map((tag) => ({ original: tag, lowercase: tag.toLowerCase() }))
-		.reduce<TagData[]>((count, tag) => {
-			const tagKey = tag.lowercase;
-			const existingTag = count.find((t) => t.lowercase === tagKey);
-			if (!existingTag) {
-				count.push({
-					original: tag.original,
-					lowercase: tag.lowercase,
-					count: 1,
-				});
-			} else {
-				existingTag.count += 1;
-			}
-			return count;
-		}, []);
+  return recipes
+    .flatMap((recipe) => recipe.data.tags || [])
+    .map((tag) => ({ original: tag, lowercase: tag.toLowerCase() }))
+    .reduce<TagData[]>((count, tag) => {
+      const tagKey = tag.lowercase;
+      const existingTag = count.find((t) => t.lowercase === tagKey);
+      if (!existingTag) {
+        count.push({
+          original: tag.original,
+          lowercase: tag.lowercase,
+          count: 1,
+        });
+      } else {
+        existingTag.count += 1;
+      }
+      return count;
+    }, []);
 };
